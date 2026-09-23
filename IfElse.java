dcfb74a185c10b1b2aca0ab93b319579622135ca
Satyam Shrivastava <satyam62621762@gmail.com>
2026-09-23 22:28:50 +0530

    Update IfElse.java

--- IfElse.java
+++ IfElse.java
@@ -3,9 +3,9 @@ public class IfElse {
         int x = 10;    
         int y = 12;    
         if(x+y < 10) {    
-            System.out.println("x + y is less than 10");    
+            System.out.println("x + y is less thn 10");    
         }   else {    
-            System.out.println("x + y is greater than 20");    
+            System.out.println("x + y is greater thn 20");    
         }    
     }    
 
